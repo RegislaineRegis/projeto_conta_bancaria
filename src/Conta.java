d3d144d7c0188d7ffffc933c94a4aa73dcc2a092
Regislaine <nanafidelis@gmail.com>
2023-04-28 15:32:36 -0300

    refactor: criacao de um menu

--- src/Conta.java
+++ src/Conta.java
@@ -38,4 +38,19 @@ public class Conta {
       this.saldo += valor;
     }    
   }
+  
+  public void visualizar() {
+		
+		System.out.println("\n\n************************************************************");
+		System.out.println("[Cem Saldo]");
+    System.out.println("[Vem ficar Cem Saldo você Também!]");
+    System.out.println("****************************************************************");
+		System.out.println("1-CONTA POUPANÇA");
+    System.out.println("2-CONTA CORRENTE");
+    System.out.println("3-CONTA ESPECIAL");
+    System.out.println("4-CONTA EMPRESA");
+    System.out.println("5-CONTA ESTUDANTIL");
+    System.out.println("6-SAIR");
+    System.out.println("\nDIGITE O CODIGO DA OPÇÃO SELECIONADA:");
+  } 
 }
