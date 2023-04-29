effd4b831e289a34771573b341b9028ec51a5305
Regislaine <nanafidelis@gmail.com>
2023-04-28 22:15:02 -0300

    refactor: alteracao construtor

--- src/Conta.java
+++ src/Conta.java
@@ -6,9 +6,11 @@ public class Conta {
   protected float saldo;
   private boolean ativo;
 
-  public Conta(int numero, String cpf) {
+  public Conta(int numero, String cpf, float saldo, boolean ativo) {
     this.numero = numero;
     this.cpf = cpf;
+    this.saldo = saldo;
+    this.ativo = ativo;
   }
 
   public int getNumero() {
@@ -16,7 +18,17 @@ public class Conta {
   }
 
   public void setNumero(int numero) {
-    this.numero = numero;
+    if(numero > 0) {
+      this.numero = numero;
+    }    
+  }
+
+  public void setSaldo(float saldo) {
+    this.saldo = saldo;      
+  }
+
+  public void setAtivo(Boolean ativo) {
+    this.ativo = ativo;      
   }
 
   public String getCpf() {
@@ -38,19 +50,8 @@ public class Conta {
       this.saldo += valor;
     }    
   }
-  
-  public void visualizar() {
-		
-		System.out.println("\n\n************************************************************");
-		System.out.println("[Cem Saldo]");
-    System.out.println("[Vem ficar Cem Saldo você Também!]");
-    System.out.println("****************************************************************");
-		System.out.println("1-CONTA POUPANÇA");
-    System.out.println("2-CONTA CORRENTE");
-    System.out.println("3-CONTA ESPECIAL");
-    System.out.println("4-CONTA EMPRESA");
-    System.out.println("5-CONTA ESTUDANTIL");
-    System.out.println("6-SAIR");
-    System.out.println("\nDIGITE O CODIGO DA OPÇÃO SELECIONADA:");
-  } 
+
+  public void correcao(int dia) {
+  }
+   
 }
